67ec8698b130da9e33ee6fe5d7eb6029fa2b8eba
Kris Lafrance <Kris.lafrance6067@gmail.com>
2019-10-13 17:03:10 -0700

    finished form event handler

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import SearchBar from "./SearchBar";
 
 const App = () => {
-	return <div>App</div>;
+	return (
+		<div className="ui container mt-3">
+			<SearchBar />
+		</div>
+	);
 };
 
 export default App;
