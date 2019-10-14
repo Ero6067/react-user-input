b498dfac7a5d0c822e662d4635cddad7953315f8
Kris Lafrance <Kris.lafrance6067@gmail.com>
2019-10-13 17:58:01 -0700

    finished component communication with callbacks

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,18 @@
 import React from "react";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-	return (
-		<div className="ui container mt-3">
-			<SearchBar />
-		</div>
-	);
-};
+class App extends React.Component {
+	onSearchSubmit(term) {
+		console.log(term);
+	}
+
+	render() {
+		return (
+			<div className="ui container mt-3">
+				<SearchBar onSubmit={this.onSearchSubmit} />
+			</div>
+		);
+	}
+}
 
 export default App;
